cd19a4c77dc7bdb9aa185c97a3d326ee73fdee6c
cybraia <27krishna2002@gmail.com>
2023-07-01 13:41:25 +0530

    imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Register from './Components/Register';
-
 
 function App() {
   return (
