c7baf3f47a4d95bc857a0a6097e9a58394b754cf
cybraia <27krishna2002@gmail.com>
2023-07-01 13:35:16 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+
 
 function App() {
   return (
     <div className="App">
-      
+       <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
