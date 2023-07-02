56f088d33ea259f5a798efb9e5d6e9cba52e6954
amritrai5757 <amritrai5757@gmail.com>
2023-07-02 06:03:50 +0530

    chatbot

---
+++ chatbot/script.js
@@ -0,0 +1,55 @@
+/*
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  const input = document.getElementById("input").value;
+  const data = {
+    model: 'davinci',
+    prompt: input + '->',
+    max_tokens: 200,
+    temperature: 0.1,
+  };
+  fetch('https://api.openai.com/v1/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '
+    },
+    body: JSON.stringify(data)
+  })
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    const output = data.choices[0].text;
+    document.getElementById('output').innerHTML = input + '<br>' + output;
+  });
+});
+*/
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+
+    const input = document.getElementById("input").value;
+    const data = {
+        model: 'davinci',
+        prompt: input + '->',
+        max_tokens: 200,
+        temperature: 0.1,
+    };
+    fetch('https://api.openai.com/v1/completions',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer '
+        },
+        body: JSON.stringify(data)
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const output = data.choices[0].text;
+        document.getElementById('output').innerHTML = input + '<br>' + output;
+    });
+});
